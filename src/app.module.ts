ba2c46344add10971544e58e9bf23f910ad58aaf
Alert175 <40724267+Alert175@users.noreply.github.com>
2021-08-26 17:41:34 +0300

    education module / controllers / services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ExampleController } from "./controllers/example.controller";
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ExampleController],
+  providers: [AppService]
 })
-export class AppModule {}
+
+export class AppModule {
+}
